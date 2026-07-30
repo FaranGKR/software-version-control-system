a2dd1cea12bc6cb96d2d2528555df0fd296ee5ae
Muhammad Haroon <mharoon.mscs16seecs@seecs.edu.pk>
2026-07-30 12:15:28 +0500

    api project

---
+++ backend/version-api-service/src/test/java/pk/edu/niit/version_system/VersionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.edu.niit.version_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
