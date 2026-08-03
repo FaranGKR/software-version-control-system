1b4dc10eeb1e2a05c4130ae37b7a01b6b211c414
Muqadas Ilyas <muqadasilyas2005@gmail.com>
2026-08-03 15:23:03 +0500

    Added logger

--- src/main/java/pk/edu/niit/version_system/Controller/UserController.java
+++ src/main/java/pk/edu/niit/version_system/Controller/UserController.java
@@ -1,4 +1,5 @@
 package pk.edu.niit.version_system.Controller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import pk.edu.niit.version_system.Entities.User;
 import org.springframework.web.bind.annotation.*;
@@ -7,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import pk.edu.niit.version_system.Services.UserService;
-
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -17,27 +18,32 @@ public class UserController {
 
     @GetMapping
     public List<User> getAll() {
+        log.info("GET/user -All Users found");
         return userService.getAll();
     }
 
     @PostMapping
     public User createEntry(@RequestBody User user) {
+        log.info("POST/user - User created");
         return userService.createEntry(user);
     }
 
     @DeleteMapping("id/{id}")
     public void deleteUser(@PathVariable int id) {
+        log.info("DELETE/user/id/{} - User deleted ",id);
          userService.deleteUserEntrybyId(id);
     }
 
     @PutMapping("id/{id}")
     public User updateUserEntry(@PathVariable int id,
                                     @RequestBody User user) {
+        log.info("UPDATE/user/id/{} - User updated ",id);
         return userService.updateUserEntryById(id, user);
     }
 
     @GetMapping("id/{id}")
     public User getUserbyId(@PathVariable int id) {
+        log.info("GET/user/id/{id} - User deleted ",id);
         return userService.getUserbyId(id);
     }
 }
